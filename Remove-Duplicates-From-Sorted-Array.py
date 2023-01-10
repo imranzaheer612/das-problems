45808b861b1ed83f88deda8e105586ee24fb2fb6
imran zaheer <izaheer612@gmail.com>
2023-01-11 01:47:40 +0500

    clean the code

--- Remove-Duplicates-From-Sorted-Array.py
+++ Remove-Duplicates-From-Sorted-Array.py
@@ -1,7 +1,7 @@
 
 class Solution:
     
-    #best
+    #If a num is unique replace it, next to the last unique num seen
     def removeDuplicates(self, nums):
         l = 1
 
@@ -12,42 +12,8 @@ class Solution:
         
         return l
 
-    # working sol
-    def removeDuplicates_1(self, nums) -> int:
-        if len(nums)==1:
-            return 1
-        
-        x = nums[0]
-        i = 0
-        while i < len(nums)-1:
-            if x == nums[1+i]:
-                del nums[1+i]
-            else:
-                x = nums[1+i]
-                i += 1
-        
-        return i+1
 
-    # pending
-    def removeDuplicates_2(self, nums) -> int:
-
-        if len(nums)==1:
-            return 1
-        
-        k = 0;
-        i = 1
-        while i < len(nums):
-            print(nums)
-            if (nums[i - 1] == nums[i]):
-                del nums[i]
-                k += 1
-                i = i - 1 
-            i += 1
-
-        return k
-
-
-
+# Driver Code
 test_list = [0,0,1,1,1,2,2,3,3,4]
 sol = Solution();
 print(sol.removeDuplicates(test_list))
