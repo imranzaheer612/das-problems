22db84ece71770aa227730cd25743c1c2d9c71e1
imran <izaheer612@gmail.com>
2023-01-14 03:54:43 +0500

    search insert pos by bin-search

---
+++ Search-Insert-Position-Binary-Search.py
@@ -0,0 +1,26 @@
+class Solution:
+    def searchInsert(self, nums, target) -> int:
+        
+        # left, right pointers
+        l, r = 0, len(nums) - 1
+        
+        while l <= r:
+            mid  = (l + r) // 2
+            
+            if target == nums[mid]:
+                return mid
+            
+            # if target is on left side
+            elif target > nums[mid]:
+                l = mid + 1
+            
+            # target on right side
+            else:
+                r = mid - 1
+
+        return l
+
+# Driver Code
+test_list = [1,3,5,6]
+sol = Solution();
+print(sol.searchInsert(test_list, 7))
