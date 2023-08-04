65c78b93e17f8b4549e8a8a37faa10d2a31c423e
imran zaheer <izaheer612@gmail.com>
2023-08-05 01:37:02 +0500

    Add new method

--- Product of Array Except Self.py
+++ Product of Array Except Self.py
@@ -23,8 +23,24 @@ class Solution:
 
         return res
 
+    def Optimized(self, nums: list[int]) -> list[int]:
+        n = len(nums)
+        res = [1 for i in range(n)]
+        
+        prefix = 1
+        for i in range(n):
+            res[i] = prefix
+            prefix *= nums[i]
+
+        postfix = 1
+        for i in range(n - 1, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
+
+        return res
+
 
 nums = [1,2,3,4]
 
 sol = Solution()
-print(sol.productExceptSelf(nums))
+print(sol.Optimized(nums))
